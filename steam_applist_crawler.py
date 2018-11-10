d98afaa377f8abca436827758ca2353ab1cfbdc3
Rama <iammastercloud@gmail.com>
2018-11-10 12:18:17 +0800

    add 20181109 applist & review

--- steam_applist_crawler.py
+++ steam_applist_crawler.py
@@ -2,7 +2,7 @@ import requests
 import json
 import csv
 
-filename = "applist_20181107"
+filename = "applist_20181109"
 
 def get_api_response(web_url):
     reponse = requests.get(web_url)
--- steam_review_crawler.py
+++ steam_review_crawler.py
@@ -5,7 +5,7 @@ import csv
 import time
 
 filename = "applist_for_review_20181031"
-result_filename = 'applist_for_review_20181107'
+result_filename = 'applist_for_review_20181109'
 
 def match_string(pattern, string):
     return re.search(pattern, string).group(0)
